3adaf3137d06ca6c96fa9f7cd77d05bfbb0bb1d7
subhambijarnia <subhambijarniya@gmail.com>
2020-12-26 20:04:29 +0530

    New

---
+++ myapi/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+from myapi.core import views
+
+urlpatterns = [
+    path('hello/', views.HelloView.as_view(), name='hello'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('admin/', admin.site.urls),
+]
+
